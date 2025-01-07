b0fff432e9e44fdb380d6f3036b52cb2b8a6d3b7
Anand Maurya <142294365+Anand50204@users.noreply.github.com>
2025-01-08 01:51:33 +0800

    Update Doctors.jsx

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -41,9 +41,15 @@ const Doctors = () => {
               <div className=' border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
                 <img onClick={() => Navigate(`/appointment/${item._id}`)} className=' bg-blue-50' src={item.image} alt="" />
                 <div className=' p-4'>
-                  <div className=' flex items-center gap-2 text-sm text-center text-green-500'>
-                    <p className=' w-2 h-2 bg-green-500 rounded-full'> </p><p>Available</p>
-                  </div>
+                  {
+                       item.available
+                       ? <div className=' flex items-center gap-2 text-sm text-center text-green-500'>
+                         <p className=' w-2 h-2 bg-green-500 rounded-full'> </p><p>Available</p>
+                          </div>
+                          : <div className=' flex items-center gap-2 text-sm text-center text-red-400'>
+                               <p className=' w-2 h-2 bg-red-400 rounded-full'> </p><p>Not Available</p>
+                           </div>
+                     }
                   <p className=' text-gray-900 text-lg font-medium'>{item.name}</p>
                   <p className=' text-gray-600 text-sm'>{item.speciality}</p>
                 </div>
